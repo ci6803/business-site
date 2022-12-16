e5260a2cdbf125f081a8d56b6d33f6c17c50cbc7
ci6803 <ci6803@gmail.com>
2022-12-16 11:02:16 +0100

    hero section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 function GetStarted() {
-  return <div>GetStarted</div>;
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div></div>
+    </div>
+  );
 }
 
 export default GetStarted;
